09a6c27eac035c93d5a00a97180e92d2e0fe5dac
Brijesk <brijeshk_raj@yahoo.com>
2020-05-21 16:04:27 +0530

    code clean up to remove debug prints and updated yml

--- test/test_functional_guardrail.py
+++ test/test_functional_guardrail.py
@@ -137,9 +137,6 @@ class TestGuardrailFunctional(unittest.TestCase):
         expec_file = os.path.join(ini_path, "test_guardrails.txt")
         with open(expec_file, 'r') as input_file:
             lines_expec = input_file.readlines()
-        print((str(lines_expec).replace("\n", "")).replace(r"\\", os.sep))
-        print("*****************************")
-        print((str(lines_test).replace("\n", "")).replace(r"\\", os.sep))
         self.assertEqual((str(lines_expec).replace("\n", "")).replace(r"\\", os.sep),
                          (str(lines_test).replace("\n", "")).replace(r"\\", os.sep))
 
